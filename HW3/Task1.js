f8822565c51248660ea9a3643f53cb94983817b1
AliakseiRauba <aliaksei.rauba@leverx.com>
2019-12-02 18:46:37 +0300

    HW3 final version
    final version

--- HW3/Task1.js
+++ HW3/Task1.js
@@ -1,7 +1,5 @@
 function onCreate(ev) {
     ev.preventDefault();
-
-
     var data = JSON.stringify({
         //"id": String(document.getElementById("cidfield").value),
         "number": String(document.getElementById("cnumberfield").value),
@@ -11,26 +9,22 @@ function onCreate(ev) {
         "dateofcreation": String(document.getElementById("cdatefield").value),
         "user": String(document.getElementById("cuserfield").value)
     });
-    console.log(data);
     xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
     xhr.addEventListener("readystatechange", function() {
         if (this.readyState === 4) {
-            alert(this.responseText);
             document.getElementById("createForm").dispatchEvent(new Event('create'));
+            console.log("Create is complete." + this.responseText);
         }
     });
-
     xhr.open("POST", "http://localhost:2403/account");
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(data);
-    alert("Created!");
 }
 
 function onRead() {
     xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
-
     xhr.addEventListener("readystatechange", function() {
         if (this.readyState === 4) {
             result = JSON.parse(this.response);
@@ -38,18 +32,58 @@ function onRead() {
             result.map(function(nthCPU) {
                 resultTBody.appendChild(parseCPUToTableRow(nthCPU));
             });
-
             var table = document.getElementById('tablebody').parentElement;
             table.replaceChild(resultTBody, document.getElementById('tablebody'));
             resultTBody.id = 'tablebody';
-            console.log('success');
+            console.log('Read is complete.')
         }
     });
-
     xhr.open("GET", "http://localhost:2403/account");
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send();
 }
+
+function onUpdate(ev) {
+    ev.preventDefault();
+    var data = JSON.stringify({
+        //"id": String(document.getElementById("cidfield").value),
+        "number": String(document.getElementById("unumberfield").value),
+        "typeofcontribution": String(document.getElementById("utypefield").value),
+        "pin": String(document.getElementById("upinfield").value),
+        "balance": String(document.getElementById("ubalancefield").value),
+        "dateofcreation": String(document.getElementById("udatefield").value),
+        "user": String(document.getElementById("uuserfield").value)
+    });
+    xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === 4) {
+            console.log("Update is complete." + this.responseText);
+        }
+    });
+    xhr.open("PUT", "http://localhost:2403/account/" + document.getElementById("uidfield").value);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data);
+
+}
+
+
+function onDelete(ev) {
+    ev.preventDefault();
+    xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === 4) {
+            console.log("Delete is complete." + this.responseText);
+        }
+    });
+
+    xhr.open("DELETE", "http://localhost:2403/account/" + document.getElementById("didfield").value);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+}
+
 
 function parseCPUToTableRow(CPUs) {
     var row = document.createElement('tr');
@@ -62,9 +96,9 @@ function parseCPUToTableRow(CPUs) {
     number.innerText = CPUs['number'];
     row.appendChild(number);
 
-    coretypeofcontribution = document.createElement('td');
-    coretypeofcontribution.innerText = CPUs['coretypeofcontribution'];
-    row.appendChild(coretypeofcontribution);
+    typeofcontribution = document.createElement('td');
+    typeofcontribution.innerText = CPUs['typeofcontribution'];
+    row.appendChild(typeofcontribution);
 
     pin = document.createElement('td');
     pin.innerText = CPUs['pin'];
@@ -82,9 +116,9 @@ function parseCPUToTableRow(CPUs) {
     user.innerText = CPUs['user'];
     row.appendChild(user);
 
+    console.log("TableBody is created.");
     return row;
 }
-
 
 
 (function() {
@@ -95,5 +129,11 @@ function parseCPUToTableRow(CPUs) {
     document.getElementById('rbutton').addEventListener(
         'click', onRead
     );
-    console.log('Handlers is set')
+    document.getElementById('ubutton').addEventListener(
+        'click', onUpdate
+    );
+    document.getElementById('dbutton').addEventListener(
+        'click', onDelete
+    );
+    console.log('Handlers is set');
 })()
