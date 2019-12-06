6a74afc4431c6346465fe649eff323f4f4b084ae
Aliaksei Rauba <aliaksei.rauba@leverx.com>
2019-12-06 21:50:17 +0300

    #1 part of HW5
    ES6 for HW3 classes

---
+++ HW5/Task_CRUD.js
@@ -0,0 +1,139 @@
+function onCreate(ev) {
+    ev.preventDefault();
+    var data = JSON.stringify({
+        //"id": String(document.getElementById("cidfield").value),
+        "number": String(document.getElementById("cnumberfield").value),
+        "typeofcontribution": String(document.getElementById("ctypefield").value),
+        "pin": String(document.getElementById("cpinfield").value),
+        "balance": String(document.getElementById("cbalancefield").value),
+        "dateofcreation": String(document.getElementById("cdatefield").value),
+        "user": String(document.getElementById("cuserfield").value)
+    });
+    xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === 4) {
+            document.getElementById("createForm").dispatchEvent(new Event('create'));
+            console.log("Create is complete." + this.responseText);
+        }
+    });
+    xhr.open("POST", "http://localhost:2403/account");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data);
+}
+
+function onRead() {
+    xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === 4) {
+            result = JSON.parse(this.response);
+            var resultTBody = document.createElement('tbody');
+            result.map(function(nthCPU) {
+                resultTBody.appendChild(parseCPUToTableRow(nthCPU));
+            });
+            var table = document.getElementById('tablebody').parentElement;
+            table.replaceChild(resultTBody, document.getElementById('tablebody'));
+            resultTBody.id = 'tablebody';
+            console.log('Read is complete.')
+        }
+    });
+    xhr.open("GET", "http://localhost:2403/account");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+}
+
+function onUpdate(ev) {
+    ev.preventDefault();
+    var data = JSON.stringify({
+        //"id": String(document.getElementById("cidfield").value),
+        "number": String(document.getElementById("unumberfield").value),
+        "typeofcontribution": String(document.getElementById("utypefield").value),
+        "pin": String(document.getElementById("upinfield").value),
+        "balance": String(document.getElementById("ubalancefield").value),
+        "dateofcreation": String(document.getElementById("udatefield").value),
+        "user": String(document.getElementById("uuserfield").value)
+    });
+    xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === 4) {
+            console.log("Update is complete." + this.responseText);
+        }
+    });
+    xhr.open("PUT", "http://localhost:2403/account/" + document.getElementById("uidfield").value);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data);
+
+}
+
+
+function onDelete(ev) {
+    ev.preventDefault();
+    xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === 4) {
+            console.log("Delete is complete." + this.responseText);
+        }
+    });
+
+    xhr.open("DELETE", "http://localhost:2403/account/" + document.getElementById("didfield").value);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+}
+
+
+function parseCPUToTableRow(CPUs) {
+    var row = document.createElement('tr');
+
+    id = document.createElement('th');
+    id.innerText = CPUs['id'];
+    row.appendChild(id);
+
+    number = document.createElement('td');
+    number.innerText = CPUs['number'];
+    row.appendChild(number);
+
+    typeofcontribution = document.createElement('td');
+    typeofcontribution.innerText = CPUs['typeofcontribution'];
+    row.appendChild(typeofcontribution);
+
+    pin = document.createElement('td');
+    pin.innerText = CPUs['pin'];
+    row.appendChild(pin);
+
+    balance = document.createElement('td');
+    balance.innerText = CPUs['balance'];
+    row.appendChild(balance);
+
+    dateofcreation = document.createElement('td');
+    dateofcreation.innerText = CPUs['dateofcreation'];
+    row.appendChild(dateofcreation);
+
+    user = document.createElement('td');
+    user.innerText = CPUs['user'];
+    row.appendChild(user);
+
+    console.log("TableBody is created.");
+    return row;
+}
+
+
+(function() {
+
+    document.getElementById('cbutton').addEventListener(
+        'click', onCreate
+    );
+    document.getElementById('rbutton').addEventListener(
+        'click', onRead
+    );
+    document.getElementById('ubutton').addEventListener(
+        'click', onUpdate
+    );
+    document.getElementById('dbutton').addEventListener(
+        'click', onDelete
+    );
+    console.log('Handlers is set');
+})()
---
+++ HW5/Task_classes.js
@@ -0,0 +1,99 @@
+let numberOfAccs;
+let accounts = [];
+
+
+//main class
+class Account {
+    constructor(id, number) {
+        this.id = id;
+        this.number = number;
+    };
+    set id(id) {
+        this._id = id;
+    };
+    get id() {
+        return this._id;
+    };
+    set number(number) {
+        this._number = number;
+    };
+    get number() {
+        return this._number;
+    };
+
+    //write info
+    getInfoAboutAccount() {
+        return (
+            "ID: " + this.id + "\n" +
+            "Number: " + this.number
+        );
+    };
+};
+
+//child classes
+class BalanceInfo extends Account {
+    constructor(balance, id, number) {
+        super(id, number);
+        this.balance = balance;
+    };
+    get Balance() {
+        return this._balance;
+    };
+    set Balance(balance) {
+        this._balance = balance;
+    };
+
+    getInfoAboutBalance() {
+        return (
+            super.getInfoAboutAccount() + "\n" +
+            "Balance: " + this.balance
+        );
+    };
+};
+
+class MoreInfo extends Account {
+    constructor(type, username, id, number) {
+        super(id, number);
+        this.username = username;
+        this.type = type;
+    };
+    get Type() {
+        return this._type;
+    };
+    set Type(new_type) {
+        this._type = type;
+    };
+    get Username() {
+        return this._username;
+    };
+    set Username(username) {
+        this._username = username;
+    };
+
+    getMoreInfo() {
+        return (
+            super.getInfoAboutAccount() + "\n" +
+            "Type: " + this.type + "\n" +
+            "Username: " + this.username
+        );
+    };
+};
+
+
+
+let account1 = new Account("3h4h45", 23);
+console.log(account1.getInfoAboutAccount());
+let account2 = new Account("4564ert5", 11);
+console.log(account2.getInfoAboutAccount());
+let accountBalance = new BalanceInfo(500, "3h4h45", 23);
+console.log(accountBalance.getInfoAboutBalance());
+let accountMoreInfo = new MoreInfo("simple", "aliakseirauba", "sdf7sdf788", 1);
+console.log(accountMoreInfo.getMoreInfo());
+
+accounts.push(account1);
+accounts.push(account2);
+accounts.push(accountBalance);
+accounts.push(accountMoreInfo);
+
+
+console.log(accounts)
