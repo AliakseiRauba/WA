f094db09f8a8412493791c843917f0bb469b813f
AliakseiRauba <aliaksei.rauba@leverx.com>
2020-01-12 18:50:42 +0300

    UI5 Demo App - Walkthrough Tutorial
    Steps 1-5 completed

--- Homeworks - NodeJS/dist/index.js
+++ Homeworks - NodeJS/dist/index.js
@@ -12,16 +12,20 @@ var _Worker = require('./Worker');
 
 var _Worker2 = _interopRequireDefault(_Worker);
 
+var _nedbPromises = require('nedb-promises');
+
+var _nedbPromises2 = _interopRequireDefault(_nedbPromises);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
-var PORT = 8080;
+var port = 3000;
 
 var users = [];
 var workers = [];
-
 var ucount = 0;
 var wcount = 0;
+var datastore = _nedbPromises2.default.create('data/db.db');
 
 var User1 = new _User2.default(1234, "User 1");
 console.log('user ' + User1.getInfoAboutUser() + ' are created:  ' + (ucount += 1));
@@ -37,15 +41,17 @@ console.log('worker ' + Worker2.getInfoAboutWork() + ' are created: ' + (wcount 
 
 users.push(User1);
 users.push(User2);
-
 workers.push(Worker1);
 workers.push(Worker2);
+
+datastore.insert(users);
+datastore.insert(workers);
 
 app.get('/info', function (req, res) {
     res.send('\n    <body>\n        <h2>Users:</h2>\n        <h4>\n            <div><br>' + User1.getInfoAboutUserToHtml() + '</br></div>\n            <div><br>' + User2.getInfoAboutUserToHtml() + '</br></div>\n        </h4>\n        <h2>Workers:</h2>\n        <h4>\n            <div><br>' + Worker1.getInfoAboutWorkToHtml() + '</br></div>\n            <div><br>' + Worker2.getInfoAboutWorkToHtml() + '</br></div>\n        </h4>\n    </body>\n    ');
 });
 
-app.listen(PORT, function () {
-    console.log('Server running at: http://localhost:' + PORT + '/');
+app.listen(port, function () {
+    console.log('Server running at: http://localhost:' + port + '/');
 });
 //# sourceMappingURL=index.js.map
